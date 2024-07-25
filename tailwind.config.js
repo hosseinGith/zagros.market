6ce54c276932ac520e99443ba6d0a34ebe4607b4
hosseinGith <hosseinderee7@gmail.com>
2024-07-26 02:32:52 +0430

    added profile & submit pages & other setting web site

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      submitPageColorBorderLowBlack: "#e7eaec",
+      submitPageColorBorderBlue: "#68a5fe",
+      lowGray: "#424750",
+      redColor: "#ec1d37",
+      whiteColor: "#fff",
+      blackColor: "#000",
+      linkColor: "#68a5fe",
+      primary: "#ef4056",
+      inputSearchBar: "#f0f0f1",
+      headerLinksBgColor: "#fde5e8",
+      lowGreenColor: "#28a745",
+      headerNavBgColor: "#f5f5f5",
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+};
